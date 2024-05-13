56ae32be7f1b0636162ac0eaf8f11381d96024bb
Guilherme China <echina725@gmail.com>
2024-05-13 09:12:00 -0300

    finishs ex1

---
+++ react-fundamentals24E2_2/guilherme_china_DR2_TP1/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Ex01 from "./pages/Ex01";
+
+export default function App() {
+    return (
+        <main>
+            <Ex01 />
+        </main>
+    )
+}
---
+++ react-fundamentals24E2_2/guilherme_china_DR2_TP1/src/pages/Ex01/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css"
+
+const Ex01 = () => {
+    return (
+        <div className="explanation">
+            <h1>O que é React?</h1>
+            <div className="separator"></div>
+            <p>
+                React é uma biblioteca JavaScript de código aberto usada para criar
+                interfaces de usuário, especialmente para aplicativos de página única (SPA).
+                Desenvolvida pelo Facebook, ela permite que os desenvolvedores construam componentes reutilizáveis que lidam com a
+                renderização eficiente dos dados na interface do usuário. O React utiliza uma abordagem baseada em componentes,
+                onde cada parte da interface é encapsulada em um componente, permitindo um desenvolvimento mais modular e organizado.
+                Além disso, o React utiliza um conceito chamado de Virtual DOM para otimizar a performance,
+                atualizando apenas as partes da interface que foram alteradas, em vez de re-renderizar toda a página.
+            </p>
+        </div>
+    )
+}
+
+export default Ex01;
