9e866e12f5dfa6454665908d6195f505a8af62fa
Guilherme China <echina725@gmail.com>
2024-05-22 20:14:13 -0300

    removes cotation front end folder

--- react-fundamentals24E2_2/cotation-frontend/src/components/Loading/index.jsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react"
-
-const Loading = () => {
-    return (
-        <h2>Loading...</h2>
-    )
-}
-
-export default memo(Loading);
--- react-fundamentals24E2_2/cotation-frontend/src/pages/ErrorBoundary/index.jsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react"
-
-const ErrorBoundary = () => {
-    return (
-        <h2>Something went wrong...</h2>
-    )
-}
-
-export default memo(ErrorBoundary);
