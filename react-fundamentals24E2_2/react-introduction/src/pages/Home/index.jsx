65222beeda6899348154a320e514364d4e7e0b2c
Guilherme China <echina725@gmail.com>
2024-05-22 18:12:23 -0300

    cleaning working directory

--- react-fundamentals24E2_2/react-introduction/src/pages/Home/index.jsx
+++ react-fundamentals24E2_2/react-introduction/src/pages/Home/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import * as S from "./style"
+import { useState, useEffect } from "react";
 
 const Home = () => {
+    const [x, setX] = useState(2);
+    const [y, setY] = useState(x);
+    useEffect(() => {
+        console.log(y)
+    }, [x])
+
     return (
         <div>
             <S.Container color="blue">Home</S.Container>
+            <button onClick={() => {
+                setX(prev => prev * 2);
+                setY(prev => prev * 2);
+            }}>click me</button>
         </div>
     )
 }
