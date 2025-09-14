cf67bbfc72a147d2b6f3e3f75db5cac8f37bb25d
Guilherme China <echina725@gmail.com>
2025-09-14 17:06:10 -0300

    rm zip

--- DesenvolvimentoWebDotnet/TP1/TP1Console/obj/Debug/net9.0/TP1.AssemblyInfo.cs
+++ DesenvolvimentoWebDotnet/TP1/TP1Console/obj/Debug/net9.0/TP1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5188d244f2295923c5030b11aa3dfe780e9c524d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fbfb89b9a120424c90a82c2aa21b2ddc15597a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
